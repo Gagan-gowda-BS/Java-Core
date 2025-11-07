1fde9e1d97c9dbda34e6ff22ba4ef6c5865385ed
Gagan-gowda-BS <gagangowdabs16@gmail.com>
2025-11-07 23:30:30 +0530

    Delete A.java

--- A.java
+++
@@ -1,9 +0,0 @@
-public class A {
-    public static void main(String[] args) {
-        System.out.println(5/2);
-        System.out.println(5.0/2);
-        System.out.println(1/2*10*5);
-        System.out.println(5%2);
-        System.out.println(3725%10);
-    }
-}
