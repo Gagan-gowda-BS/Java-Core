a61bf759f603050ae02d339d618430bfdd64fde7
Gagan-gowda-BS <gagangowdabs16@gmail.com>
2025-11-10 18:53:48 +0530

    Add files via upload

---
+++ Datatypes&variables/Datatypes.java
@@ -0,0 +1,21 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        int age=22;
+        double salary=5000.00;
+        char grade='A';
+        boolean isemployed=true;
+        byte level=5;
+        long population=780000L;
+        float pi=3.14f;
+        //non primitive datatypes
+        String name="gagan";
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("Salary:"+salary);
+        System.out.println("Grade:"+grade);
+        System.out.println("Employed:"+ isemployed);
+        System.out.println("Level:"+level);
+        System.out.println("Population:"+population);
+        System.out.println("PI:"+pi);
+    }
+}
