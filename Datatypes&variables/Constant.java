0247b1bfdef8c35f2fd73fa513a9f0606e0db693
Gagan-gowda-BS <gagangowdabs16@gmail.com>
2025-11-10 18:53:23 +0530

    Add files via upload

---
+++ Datatypes&variables/Constant.java
@@ -0,0 +1,8 @@
+public class Constant {
+    public static void main(String[] args) {
+        final double PI=3.144;
+        final int MAX_USER=100;
+        System.out.println("pi:"+PI);
+        System.out.println("max users allowed"+MAX_USER);
+    }
+}
