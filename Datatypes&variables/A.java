38b318bb656c5563e6fafc24a5d58dcf3fe56ada
Gagan-gowda-BS <gagangowdabs16@gmail.com>
2025-11-10 18:52:18 +0530

    Add files via upload

---
+++ Datatypes&variables/A.java
@@ -0,0 +1,10 @@
+public class A {
+    static int i=1;
+    public static void main(String[] args) {
+        System.out.println(A.i);
+        A.i=2;
+        System.out.println(A.i);
+        A.i=5;
+        System.out.println(A.i);
+    }
+}
