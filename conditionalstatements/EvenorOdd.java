98bf8ea908201d115ef7c6c7613aa18ab1fd0938
Gagan-gowda-BS <gagangowdabs16@gmail.com>
2025-11-11 21:21:59 +0530

    Add files via upload

---
+++ conditionalstatements/EvenorOdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class EvenorOdd {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the num");
+        int n=sc.nextInt();
+        if (n%2==0){
+            System.out.println("It is even num");
+        }
+        else{
+            System.out.println("it is odd num");
+        }
+        }
+    }
+
