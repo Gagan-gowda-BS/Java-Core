fcd220e3dc4c421e3a78a0808b968939ed76d5ba
Gagan-gowda-BS <gagangowdabs16@gmail.com>
2025-11-11 21:23:21 +0530

    Add files via upload

---
+++ conditionalstatements/weekday.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class weekday {
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a week day num");
+        int d=sc.nextInt();
+        switch(d){
+            case 1:
+            System.out.println("sunday");
+            break;
+            case 2:
+            System.out.println("monday");
+            break;
+            case 3:
+            System.out.println("tuesday");
+            break;
+            case 4:
+            System.out.println("wednesday");
+            break;
+            case 5:
+            System.out.println("thursday");
+            break;
+            case 6:
+            System.out.println("friday");
+            break;
+            case 7:
+            System.out.println("saturday");
+            break;
+            default:
+            System.out.println("invalid input");
+        }
+    }
+}
