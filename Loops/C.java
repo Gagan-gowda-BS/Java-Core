646668a1c39e824ab29f11ee57674047d5b47e27
Gagan-gowda-BS <gagangowdabs16@gmail.com>
2025-11-12 23:16:39 +0530

    Add files via upload

---
+++ Loops/C.java
@@ -0,0 +1,10 @@
+public class C {
+    public static void main(String[] args) {
+        int i=0;
+        for(i=1;i<=3;i++){
+            System.out.println(i);
+            System.out.println(i);
+            System.out.println(i);
+        }
+    }
+}
