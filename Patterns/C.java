977e92924548e2def2d406b518ffd8fe5a359d43
Gagan-gowda-BS <gagangowdabs16@gmail.com>
2025-11-16 19:20:18 +0530

    Add files via upload

---
+++ Patterns/C.java
@@ -0,0 +1,14 @@
+public class C {
+        public static void main(String[] args) {
+            for(int i=1;i<=5;i++){
+                char c='A';
+                for(int j=1;j<=1;j++){
+                    System.out.print(c);
+                    c++;
+                }
+                System.out.println();
+                }
+            }
+        }
+    
+
